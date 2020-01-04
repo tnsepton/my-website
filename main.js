f1c7f7528c8597052e1b9a512d2426b596375ffa
tnsepton <thomas.septon@gmail.com>
2020-01-04 07:10:34 +0000

    Wrote About Me Content

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const aboutMe = function() {
     const title = document.createElement('h1');
     var paragraphs = [];
     const content = [
-        'alsdjflkajsldkfjasdk',
-        'lakdjflkajsdfklajsdflkj'
+        'I started working in web development in November of 2018. I fell in love with it immediately. While I was in school, I changed my major time and time again. I had a very difficult time deciding what I wanted to do for the rest of my life. Working in web development was the best thing that could’ve happened to me when it did.',
+        
+        'Coding is something that really makes sense to me. The math and the logic just click in my mind. I love the variety of challenges that I get to solve every day. I love being able to find multiple solutions to the same problem, eventually coming to the best solution for the particular problem. I am very happy to be working in the field, and my dream is to contribute to the field in a major way some day.'
     ];
 
     title.textContent = 'About Me';
